1a96d011007399b23e8305f7bf217d1584b88e91
Rodolfo Porras <porrasrodolfo7@gmail.com>
2025-12-16 00:11:30 +0000

    generador de excusas

--- src/app.js
+++ src/app.js
@@ -8,4 +8,27 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+    
+   
+
+    let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+    let action = ['ate', 'peed', 'crushed', 'broke'];
+    let what = ['my homework', 'my phone', 'the car'];
+    let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+  
+
+    let randomWho = who [Math.floor(Math.random() *4)]
+    console.log (randomWho)
+
+    let randomAction = action[Math.floor(Math.random() *4)]
+    console.log (randomAction)
+
+    let randomWhat = what[Math.floor(Math.random() *3)]
+    console.log (randomWhat)
+
+    let randomWhen = when[Math.floor(Math.random() *5)]
+    console.log (randomWhen)
+
+    let excusa =(randomWho+" "+randomAction+" "+randomWhat+" "+randomWhen)
+    document.getElementById("excuse").innerHTML=excusa
 };
